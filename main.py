bc66565d88347eccd187f593ee6e7ea418c2f823
CrazyChr1504 <chris95504@gmail.com>
2022-02-11 13:35:18 +0100

    Character Fight Func

--- main.py
+++ main.py
@@ -1,13 +1,32 @@
-from pyparsing import Char
-from resourses import Character
+from resourses import Character, Goblin
+import random
+
+def fight(fighter : Character, enemies : list):
+
+    while not len(enemies) == 0:
+        fighter_target = random.choice(enemies)
+        fighter_target.take_damage(fighter.attack())
+        if fighter_target.get_health() == 0:
+            print("\nTarget has died!")
+            enemies.remove(fighter_target)
+            if len(enemies) == 0: break
+            
+    print(f"Fight is over {fighter.name} won!")
 
 def main():
+    enemies = []
     nick = Character("Nick", 15, 3, 1)
     emy = Character("Emy", 20, 6, 5)
 
     print(nick)
     print()
     print(emy)
-    
+
+    enemies.append(Goblin(1))
+    print("\nGoblins")
+    print(enemies[0])
+
+    fight(emy, enemies)
+
 if __name__ == "__main__":
     main()
